4d5a97f749e7dee2ab82770fa3e1011874cbe72a
IonutVladeanu <124895226+IonutVladeanu@users.noreply.github.com>
2023-02-10 13:56:22 +0200

    Update Ceaser_Cipher.py

--- Ceaser_Cipher.py
+++ Ceaser_Cipher.py
@@ -1,4 +1,13 @@
-# Ceaser cipher script
+# Ceaser cipher encryption and decryption program script
+# The Caesar cipher is an ancient encryption algorithm used by Julius Caesar. It 
+# encrypts letters by shifting them over by a 
+# certain number of places in the alphabet. We 
+# call the length of shift the key. For example, if the 
+# key is 3, then A becomes D, B becomes E, C becomes 
+# F, and so on. To decrypt the message, you must shift 
+# the encrypted letters in the opposite direction. This 
+# program lets the user encrypt and decrypt messages 
+# according to this algorithm.
 user_choice = input("Do you want to (e)ncrypt or (d)ecrypt: ")
 
 key = int(input("Please enter the key (0 to 25) to use: "))
